55e6596cec9c2a50edbb72616c56b3e8dabffbc1
charlie890414 <charlie890414@gmail.com>
2021-10-01 22:14:02 +0800

    load nasa obj

--- src/index.js
+++ src/index.js
@@ -1,104 +1,109 @@
 import * as THREE from 'three';
 import {
-    OBJLoader
+  OBJLoader
 } from 'three/examples/jsm/loaders/OBJLoader.js';
 import {
-    MTLLoader
+  MTLLoader
 } from 'three/examples/jsm/loaders/MTLLoader.js';
 import {
-    OrbitControls
+  OrbitControls
 } from "three/examples/jsm/controls/OrbitControls";
 
-function main() {
-    const canvas = document.querySelector('#main');
-    const renderer = new THREE.WebGLRenderer({canvas});
-  
-    const fov = 45;
-    const aspect = 2;  // the canvas default
-    const near = 0.1;
-    const far = 100;
-    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    camera.position.set(0, 10, 20);
-  
-    const controls = new OrbitControls(camera, canvas);
-    controls.target.set(0, 5, 0);
-    controls.update();
-  
-    const scene = new THREE.Scene();
-    scene.background = new THREE.Color('black');
-  
-    {
-      const planeSize = 40;
-  
-      const loader = new THREE.TextureLoader();
-      const texture = loader.load('https://threejsfundamentals.org/threejs/resources/images/checker.png');
-      texture.wrapS = THREE.RepeatWrapping;
-      texture.wrapT = THREE.RepeatWrapping;
-      texture.magFilter = THREE.NearestFilter;
-      const repeats = planeSize / 2;
-      texture.repeat.set(repeats, repeats);
-  
-      const planeGeo = new THREE.PlaneGeometry(planeSize, planeSize);
-      const planeMat = new THREE.MeshPhongMaterial({
-        map: texture,
-        side: THREE.DoubleSide,
-      });
-      const mesh = new THREE.Mesh(planeGeo, planeMat);
-      mesh.rotation.x = Math.PI * -0.5;
-      scene.add(mesh);
-    }
-  
-    {
-      const skyColor = 0xB1E1FF;  // light blue
-      const groundColor = 0xB97A20;  // brownish orange
-      const intensity = 1;
-      const light = new THREE.HemisphereLight(skyColor, groundColor, intensity);
-      scene.add(light);
-    }
-  
-    {
-      const color = 0xFFFFFF;
-      const intensity = 1;
-      const light = new THREE.DirectionalLight(color, intensity);
-      light.position.set(0, 10, 0);
-      light.target.position.set(-5, 0, 0);
-      scene.add(light);
-      scene.add(light.target);
-    }
-  
-    {
-      const objLoader = new OBJLoader();
-      objLoader.load('https://threejsfundamentals.org/threejs/resources/models/windmill/windmill.obj', (root) => {
-        scene.add(root);
-      });
-    }
-  
-    function resizeRendererToDisplaySize(renderer) {
-      const canvas = renderer.domElement;
-      const width = canvas.clientWidth;
-      const height = canvas.clientHeight;
-      const needResize = canvas.width !== width || canvas.height !== height;
-      if (needResize) {
-        renderer.setSize(width, height, false);
-      }
-      return needResize;
-    }
-  
-    function render() {
-  
-      if (resizeRendererToDisplaySize(renderer)) {
-        const canvas = renderer.domElement;
-        camera.aspect = canvas.clientWidth / canvas.clientHeight;
-        camera.updateProjectionMatrix();
-      }
-  
-      renderer.render(scene, camera);
-  
-      requestAnimationFrame(render);
-    }
-  
-    requestAnimationFrame(render);
+const MAXSIZE = 50;
+let scene;
+let camera;
+let canvas;
+let renderer;
+let mainOBJ;
+
+function addLight() {
+  const color = 0xFFFFFF;
+  const intensity = 1;
+  const light = new THREE.DirectionalLight(color, intensity);
+  light.position.set(0, 100, 0);
+  light.target.position.set(0, -100, 0);
+  scene.add(light);
+  scene.add(light.target);
+}
+
+function addCamera() {
+  const fov = 45;
+  const aspect = 2;
+  const near = 0.01;
+  const far = 1000;
+  camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.set(0, 100, 0);
+  camera.lookAt(new THREE.Vector3(0, 0, 0))
+}
+
+function addControls() {
+  const controls = new OrbitControls(camera, canvas);
+  controls.update();
+}
+
+function addAxes() {
+  const axesHelper = new THREE.AxesHelper(100);
+  scene.add(axesHelper);
+}
+
+function animation() {
+  if (mainOBJ) {
+    mainOBJ.rotateX(Math.PI / 180);
+    mainOBJ.rotateY(Math.PI / 180);
+    mainOBJ.rotateZ(Math.PI / 180);
   }
-  
-  main();
-  
+}
+
+function caculateScale(obj) {
+  const box = new THREE.Box3().setFromObject(obj);
+  const x = box.max.x - box.min.x;
+  const y = box.max.y - box.min.y;
+  const z = box.max.z - box.min.z;
+  const longest = Math.max(x, y, z);
+  return MAXSIZE / longest;
+}
+
+function resizeRendererToDisplaySize(renderer) {
+  const canvas = renderer.domElement;
+  const width = canvas.clientWidth;
+  const height = canvas.clientHeight;
+  const needResize = canvas.width !== width || canvas.height !== height;
+  if (needResize) {
+    renderer.setSize(width, height, false);
+  }
+  return needResize;
+}
+
+function render() {
+  if (resizeRendererToDisplaySize(renderer)) {
+    const canvas = renderer.domElement;
+    camera.aspect = canvas.clientWidth / canvas.clientHeight;
+    camera.updateProjectionMatrix();
+  }
+  animation();
+  renderer.render(scene, camera);
+  requestAnimationFrame(render);
+}
+
+function main() {
+  canvas = document.querySelector('#main');
+  renderer = new THREE.WebGLRenderer({ canvas });
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color('black');
+
+  addCamera()
+  addLight()
+  // addControls();
+  // addAxes()
+
+  const objLoader = new OBJLoader();
+  objLoader.load('kleo.obj', (obj) => {
+    mainOBJ = obj;
+    obj.scale.x = obj.scale.y = obj.scale.z = caculateScale(obj);
+    scene.add(obj);
+  });
+
+  requestAnimationFrame(render);
+}
+
+main();
